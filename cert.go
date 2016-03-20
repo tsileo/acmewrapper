9029eb0f77885b21f667f0e1d58299b74d472a7c
Jaime Pillora <dev@jpillora.com>
2016-03-20 19:59:30 +1100

    Reinclude file-based backup

--- cert.go
+++ cert.go
@@ -3,6 +3,8 @@ package acmewrapper
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/xenolf/lego/acme"
@@ -13,21 +15,37 @@ import (
 // If the files already exist, it renames the old versions by adding .bak to them. This makes
 // sure that a little accident doesn't cause too much damage.
 func (w *AcmeWrapper) writeCert(certfile, keyfile string, crt acme.CertificateResource) (err error) {
-	//only create a backup version if we're definitely saving to disk
-	if w.Config.SaveFileCallback == nil {
-		if err := w.backupAndSaveFile(certfile, crt.Certificate); err != nil {
-			return err
-		}
-		if err := w.backupAndSaveFile(keyfile, crt.PrivateKey); err != nil {
-			return err
-		}
-	} else {
+	//If user has provided custom file handling, skip backups
+	if w.Config.SaveFileCallback != nil {
 		if err := w.saveFile(certfile, crt.Certificate); err != nil {
 			return err
 		}
 		if err := w.saveFile(keyfile, crt.PrivateKey); err != nil {
 			return err
 		}
+		return nil
+	}
+	//If the files already exist, move them to backup
+	err = os.Rename(certfile, certfile+".bak")
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	err = os.Rename(keyfile, keyfile+".bak")
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	err = ioutil.WriteFile(certfile, crt.Certificate, 0600)
+	if err != nil {
+		os.Rename(certfile+".bak", certfile)
+		os.Rename(keyfile+".bak", keyfile)
+		return err
+	}
+	err = ioutil.WriteFile(keyfile, crt.PrivateKey, 0600)
+	if err != nil {
+		os.Remove(certfile)
+		os.Rename(certfile+".bak", certfile)
+		os.Rename(keyfile+".bak", keyfile)
+		return err
 	}
 	return nil
 }
